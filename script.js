4ff05010fa0bd699b9527b658d5080329839e7c4
ねずみくん <103794002+nezumi0627@users.noreply.github.com>
2023-09-14 18:12:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-window.addEventListener("scroll", function() {
-    var scrollButton = document.getElementById("scrollBtn");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollButton.style.display = "block";
-    } else {
-        scrollButton.style.display = "none";
-    }
-});
 
-function scrollToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
