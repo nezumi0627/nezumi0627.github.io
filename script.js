51bd1a5d69132437b35460c4b1186e3d0f60da48
ねずみくん <103794002+nezumi0627@users.noreply.github.com>
2024-02-13 08:51:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// 404エラーが発生した場合にリダイレクト
+window.addEventListener("error", function(e) {
+    if (e && e.target && e.target.src && e.target.src.endsWith("style.css")) {
+        // 404エラーが style.css から発生した場合のリダイレクト
+        window.location.href = "https://nezumi0627.github.io/404.github.io";
+    }
+});
 
